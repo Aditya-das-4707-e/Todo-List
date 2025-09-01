c4adbe68a48765022fb7f918b926e0934cc57452
Aditya Das <adityadas99906@gmail.com>
2025-09-01 09:13:20 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav>
-        <ul className="flex"></ul>
+    <nav className='flex justify-between bg-slate-700 text-white py-4'>
+        <div className="logo">
+            <span className='font-bold text-xl mx-8'>Task</span>
+        </div>
+        <ul className="flex gap-5 mx-9">
+            <li className='cursor-pointer'>Home</li>
+            <li className='cursor-pointer'>Your Task</li>
+        </ul>
     </nav>
   )
 }
