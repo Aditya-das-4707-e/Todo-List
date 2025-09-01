79793d297a21ab7dbce8dfdeedc24a461844ed64
Aditya Das <adityadas99906@gmail.com>
2025-09-01 14:55:08 +0530

    new work

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,15 @@ function App() {
   return (
     <>
       <Navbar />
-      <div className="container mx-auto">
-        <div className="bg-red-600">
-          hey i am div 
-        </div>
+      <div className="container bg-indigo-100 mx-auto m-5 rounded-xl p-5">
+        
+          <h1 className='text-xl font-bold'>Your Todos</h1>
+          <div className="todos">
+            <div className="todo">
+              
+            </div>
+          </div>
+        
       </div>
     </>
   )
