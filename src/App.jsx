bc0bd4cda0472483aae053f626fddb67f94cb2db
Aditya Das <adityadas99906@gmail.com>
2025-09-01 18:14:22 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,20 @@ function App() {
   return (
     <>
       <Navbar />
-      <div className="container bg-indigo-100 mx-auto m-5 rounded-xl p-5">
-        
-          <h1 className='text-xl font-bold'>Your Todos</h1>
+      <div className="container bg-indigo-100 mx-auto m-5 rounded-xl p-5 min-h-[80vh]">
+        <div className="addTodo">
+          <h2 className='text-lg font-bold'>Add a todo</h2>
+          <input type="text" name="" id="" />
+          <button>Add</button>
+        </div>
+          <h2 className='text-xl font-bold'>Your Todos</h2>
           <div className="todos">
-            <div className="todo">
+            <div className="todo flex">
+              <div className="text">Lorem ipsum dolor sit amet consectetur adipisicing.</div>
+                <div className="buttons">
+                  <button>Edit</button>
+                  <button>Delete</button>
+                </div>
               
             </div>
           </div>
